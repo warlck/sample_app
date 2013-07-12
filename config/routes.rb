f1f5bca5b1c6ff73be93ea86c12e43c264ff8458
warlck <addick@me.com>
2013-07-12 17:45:30 +0800

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ SampleApp::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  # Sample resource route (maps HTTP verbs to controller actions automatically)
   #   resources :products
 
   # Sample resource route with options:
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,21 +1,27 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
 
-  let(:base_title) {"Ruby on Rails Tutorial Sample App" }
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
 
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-		visit '/static_pages/home'
-		page.should have_selector('h1', :text => 'Sample App');
+  		visit '/static_pages/home'
+  		page.should have_selector('h1', :text => 'Sample App')              
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
     	visit '/static_pages/home'
     	page.should have_selector('title',
-			:text => "#{base_title} | Home")          
+			:text => "#{base_title} ")          
     end
+
+    it "should not have custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+
   end
 
   describe "Help page" do
@@ -34,15 +40,15 @@ describe "StaticPages" do
 
   describe "About page" do 
 
-  	it "should have content 'About us'" do
-  		visit '/static_pages/about'
-  		page.should have_selector('h1',:text => 'About Us')
-  	end
+    it "should have content 'About us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1',:text => 'About Us')
+    end
 
-  	it "should have the right title" do
-    	visit '/static_pages/about'
-    	page.should have_selector('title',
-			:text => "#{base_title} | About")          
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', 
+                    :text => "#{base_title} | About")                               
     end
   end
 
@@ -55,9 +61,8 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/contact'
-      page.should have_selector('title', 
-      :text => "#{base_title} | Contact")
-    end
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end  
   end
 
 end
